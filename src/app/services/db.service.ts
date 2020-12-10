dbc1c494eda39d741df945c54af891e4d38c6f03
Raul <raulcastellanosh@hotmail.com>
2020-12-09 22:14:22 -0600

    db service

---
+++ src/app/services/db.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+
+  constructor(
+    private afs: AngularFirestore,
+  ) { }
+
+  getSection(idSection: number){
+    return this.afs.collection('supertemas', ref => ref.where('idTema', '==', idSection)).snapshotChanges()
+  }
+}
