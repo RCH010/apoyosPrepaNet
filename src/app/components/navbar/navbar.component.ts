2ff6d2e9c2f1ad9d46d0c868c1d1c981f16b657a
Raul <raulcastellanosh@hotmail.com>
2020-12-09 22:16:52 -0600

    Updated navbar depending on user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  usertype: string;
+  userUID: Observable<any>;
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.usertype = localStorage.getItem('uid');
+    if (!this.usertype) {
+      this.usertype = "normal"
+    }
   }
 
+  logout(){
+    this.authService.singOut().then(res =>{
+      this.router.navigateByUrl('/home').then(res => {
+        localStorage.clear()
+        location.reload()
+      })
+    })
+  }
 }
